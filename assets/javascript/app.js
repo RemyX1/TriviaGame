5ba4bc7803202cff8d4a9a49a43f8794019fe881
Remy <remington49@rocketmail.com>
2019-01-14 16:41:20 -0500

    test

---
+++ assets/javascript/app.js
@@ -0,0 +1,25 @@
+var questions = {
+   Entry1: { Q: "2 + 2 = ?",
+    A: 4
+
+    } 
+
+   , Entry2: {
+
+        Q: "4 + 4 = ?",
+        A: 8
+    }
+
+    
+};
+var X = 2;
+console.log(question )
+
+ prompt(questions.Entry1.Q)
+if (Y === questions.Entry1.A) {
+
+alert("correct")
+
+}
+
+
